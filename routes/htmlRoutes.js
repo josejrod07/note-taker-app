b0db1a69399f29d180951363cc575e24457fef8a
Jose <josejrod07@gmail.com>
2023-06-25 14:31:23 -0400

    added htmlRoutes connection to server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,5 @@
-const router = require("express").Router();
 const path = require("path");
+const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
@@ -8,3 +8,5 @@ router.get("/notes", (req, res) => {
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+module.exports = router;
